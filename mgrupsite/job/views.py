f61826ad3f8f2e455cec46ed56c945fdec6e3814
vvlxvt <vvlx@tutanota.com>
2024-04-17 15:03:51 +0400

    add handling exception for EmptyPage

--- mgrupsite/job/views.py
+++ mgrupsite/job/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -7,7 +7,10 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1) # Если параметра page нет в GET-параметрах запроса, то мы используем 1
-    posts = paginator.page(page_number) # page возвращает объект Page, который хранится в переменной posts
+    try:
+        posts = paginator.page(page_number) # page возвращает объект Page, который хранится в переменной posts
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'job/post/list.html', {'posts': posts})
 
 
