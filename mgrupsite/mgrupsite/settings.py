041e8e5db3c2c68afbc3e6c9eb3e9819887f6599
vvlxvt <vvlx@tutanota.com>
2024-05-01 15:38:07 +0400

    SQLite was changed to PostgreSQL -> settings.py

--- mgrupsite/mgrupsite/settings.py
+++ mgrupsite/mgrupsite/settings.py
@@ -83,21 +83,21 @@ WSGI_APPLICATION = 'mgrupsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'job',
-#         'USER': 'job',
-#         'PASSWORD': 'X161Y_O',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'job',
+        'USER': 'job',
+        'PASSWORD': 'X161Y_O',
+    }
+}
 
 INTERNAL_IPS=['127.0.0.1',]
 
