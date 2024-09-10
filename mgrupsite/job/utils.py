63744a123f07ae8a3c266719a5ae76d840a41162
vvlxvt <vvlx@tutanota.com>
2024-09-10 14:13:24 +0400

    segregated navbar.html from base.html; nav menu is presented as a dictionary in utils.py, added menu in context_processors

---
+++ mgrupsite/job/utils.py
@@ -0,0 +1,33 @@
+namespace = 'job'
+
+menu = {
+    'about': {'title': 'О нас', 'url_name': f'{namespace}:article_list'},
+    'services': {
+        'title': 'Услуги',
+        'url_name': f'{namespace}:post_list',
+        'submenus': [
+            {'title': 'Покраска', 'url_name': f'{namespace}:article_list'},
+            {'title': 'Очистка', 'url_name': f'{namespace}:article_list'},
+            {'title': 'Обработка', 'url_name': f'{namespace}:article_list'},]
+    },
+    'articles': {'title': 'Статьи', 'url_name': f'{namespace}:article_list'},
+    'projects': {'title': 'Проекты', 'url_name': f'{namespace}:article_list'},
+}
+
+
+class DataMixin:
+    paginate_by = 3
+    title_page = None
+    extra_context = {}
+    cat_selected = None
+
+    def __init__(self):
+        if self.title_page:
+            self.extra_context['title'] = self.title_page
+        if self.cat_selected is not None:
+            self.extra_context['cat_selected'] = self.cat_selected
+
+    def get_mixin_context(self, context, **kwargs):
+        context['cat_selected'] = None
+        context.update(**kwargs)
+        return context
