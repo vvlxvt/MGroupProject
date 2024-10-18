f53d6b64bc4d3953261e8033b1a5bc65ac11b0f4
vvlxvt <vvlx@tutanota.com>
2024-10-18 16:34:04 +0400

    added project_list.html with css for carousel and corrected navbar with logo

--- mgrupsite/mgrupsite/settings.py
+++ mgrupsite/mgrupsite/settings.py
@@ -17,6 +17,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
