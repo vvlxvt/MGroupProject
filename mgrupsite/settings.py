6daaaf9a3d06800fec2d78d744edfc5f15192fe1
vvlxvt <vvlx@tutanota.com>
2025-02-04 07:21:18 +0400

    11

--- mgrupsite/settings.py
+++ mgrupsite/settings.py
@@ -6,7 +6,7 @@ env = Env()
 env.read_env()
 
 SECRET_KEY=env('SECRET_KEY')
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = [] # для разработки
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['mgroup-vvlxvt.amvera.io', 'localhost'])
